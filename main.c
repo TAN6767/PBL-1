c4ca211defa18a72220870e3cea82ccc6efd1ed3
TAN6767 <162009838+TAN6767@users.noreply.github.com>
2024-06-08 20:16:43 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,35 @@
-#include "pbl1.h"
-#include <windows.h>
+#include<stdio.h>
+#include "pbl1_function.h"
 
 int main()
 {
     SetConsoleOutputCP(65001);
-    RunProgram();
+    int choice;
+    system("cls");
+    printf("1. Đăng nhập\n");
+    printf("2. Đăng kí tài khoản\n");
+    printf("3. Thoát chương trình\n");
+    printf("\nNhập lựa chọn của bạn: ");
+    scanf("%d",&choice);
+    getchar();
+    switch (choice)
+    {
+    case 1:
+        system("cls");
+        if (Login() == true)
+        {
+        Start();
+        MENU();
+        }
+        break;
+    case 2:
+        system("cls");
+        Register();
+        Start();
+        MENU();
+        break;
+    case 3:
+        printf("Thoát chương trình thành công!\n");
+        return 0;
+    }
 }
